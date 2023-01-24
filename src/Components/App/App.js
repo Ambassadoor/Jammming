05a433a55a5521bbe82e43f2974af9a51a6083fc
Caleb <calebpttmn@gmail.com>
2023-01-24 16:42:21 -0600

    added search method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -52,6 +53,10 @@ class App extends React.Component {
     const trackURIs = this.state.playlistTracks.map((track) => track.uri);
   }
 
+  search(searchTerm) {
+    console.log(searchTerm);
+  }
+
   render() {
     return (
       <div>
@@ -59,7 +64,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar></SearchBar>
+          <SearchBar onSearch={this.search}></SearchBar>
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
@@ -70,6 +75,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             ></Playlist>
           </div>
         </div>

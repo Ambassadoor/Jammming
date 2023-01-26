9aad1088a041306d59b79f8eefde633e6b4c542c
Caleb <calebpttmn@gmail.com>
2023-01-26 14:33:54 -0600

    added functionality to obtain spotify access token

---
+++ src/util/Spotify.js
@@ -0,0 +1,26 @@
+let accessToken;
+const clientID = '6c33ba429c094c23b11b79d8dbf113cc';
+const redirectURI = "http://localhost:3000";
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessURL = 'https://accounts.spotify.com/authorize?client_id='+clientID+'&response_type=token&scope=playlist-modify-public&redirect_uri='+redirectURI;
+            window.location = accessURL;
+        }
+        }
+    }
+export default Spotify;
